1b22914f3eea699438fb1f37c72abc5ca602f5dd
Nazmul Haque <nazmul45666@gmail.com>
2023-11-10 23:29:55 +0600

    change

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const http = require("node:http");
+const http = require("http");
 var express = require("express");
 var bodyParser = require("body-parser");
-var cors=require('cors')
+var cors = require("cors");
 // define port
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8080;
 }
 
-
 const app = express();
-app.use(cors())
+app.use(cors());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,15 +24,19 @@ app.get("/", (req, res) => {
 app.post("/api/v1", (req, res) => {
   url = req.body.link;
   console.log(url);
-  const request=http.get(url,(rsp)=>{
-    res.status(rsp.statusCode).json({ status: `${rsp.statusCode}`,url:url });
-  //   console.log('headers:', res.headers);
-  })
-  request.on('socket', (s) => { s.setTimeout(4000, () => { s.destroy(); })});
-    
-  request.on("error", (e) => {
-      res.json(e);
+  const request = http.get(url, (rsp) => {
+    res.status(rsp.statusCode).json({ status: `${rsp.statusCode}`, url: url });
+    //   console.log('headers:', res.headers);
+  });
+  request.on("socket", (s) => {
+    s.setTimeout(4000, () => {
+      s.destroy();
     });
+  });
+
+  request.on("error", (e) => {
+    res.json(e);
+  });
 });
 
 app.listen(port, console.log("working...", port));
